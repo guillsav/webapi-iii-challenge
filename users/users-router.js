239fd88e2308648993b9dc98ca88f354bb0d69db
guillsav <guillsav@gmail.com>
2019-04-24 16:36:00 -0400

    Get route to retrieve a single user done

--- users/users-router.js
+++ users/users-router.js
@@ -3,13 +3,27 @@ const userDb = require('../data/helpers/userDb.js');
 
 const router = express.Router();
 
-// GET Route to get all the users from the daatabase
+// GET Route to get all the users from the daatabase.
 router.get('/', async (req, res) => {
   try {
     const users = await userDb.get(req.query);
     res.status(200).json(users);
   } catch (error) {
-    res.status(500).json({errorMessage: 'Error retrieving the users'});
+    res.status(500).json({errorMessage: 'Error getting the users'});
+  }
+});
+
+// GET route to get a single user based on the id parameter in the url.
+router.get('/:id', async (req, res) => {
+  try {
+    const user = await userDb.getById(req.params.id, req.body);
+    if (!user) {
+      res.status(404).json({errorMessage: 'User not found'});
+    } else {
+      res.status(200).json(user);
+    }
+  } catch (error) {
+    res.status(500).json({errorMessage: 'Error getting the user'});
   }
 });
 
