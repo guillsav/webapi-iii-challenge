a2af81e9b8f766d564663d1b88e590802793ed1c
guillsav <guillsav@gmail.com>
2019-04-24 14:11:08 -0400

    Server built

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// code away!
+const express = require('express');
+const morgan = require('morgan');
+
+const server = express();
+
+const PORT = 5000;
+
+server.listen(PORT, () => {
+  console.log(`Server is listening on port: ${PORT}`);
+});
