53340a73b9d82607c1097cff76a10b3fc88233d2
guillsav <guillsav@gmail.com>
2019-04-24 16:19:30 -0400

    PUT route to update a post done

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -4,33 +4,7 @@ const userDb = require('../data/helpers/userDb.js');
 
 const router = express.Router();
 
-// GET route to receive all the posts
-router.get('/', async (req, res) => {
-  try {
-    const posts = await postDb.get(req.query);
-    if (posts) {
-      res.status(200).json(posts);
-    }
-  } catch (error) {
-    res.status(500).json({errorMessage: 'Error retrieving the posts'});
-  }
-});
-
-// Get route to receive on post by the id of the post
-router.get('/:id', async (req, res) => {
-  try {
-    const post = await postDb.getById(req.params.id, req.body);
-    if (post) {
-      res.status(200).json(post);
-    } else {
-      res.status(404).json({errorMessage: 'Post not found'});
-    }
-  } catch (error) {
-    res.status(500).json({errorMessage: 'Error retrieving the post'});
-  }
-});
-
-// Post route to create a new post
+// POST route to create a new post
 router.post('/', async (req, res) => {
   try {
     if (req.body.text !== '' && req.body.user_id !== '') {
@@ -44,6 +18,53 @@ router.post('/', async (req, res) => {
   }
 });
 
+// GET route to receive all the posts
+router.get('/', async (req, res) => {
+  try {
+    const posts = await postDb.get(req.query);
+    if (posts) {
+      res.status(200).json(posts);
+    }
+  } catch (error) {
+    res.status(500).json({errorMessage: 'Error retrieving the posts'});
+  }
+});
+
+// GET route to receive on post by the id of the post
+router.get('/:id', async (req, res) => {
+  try {
+    const post = await postDb.getById(req.params.id, req.body);
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res.status(404).json({errorMessage: 'Post not found'});
+    }
+  } catch (error) {
+    res.status(500).json({errorMessage: 'Error retrieving the post'});
+  }
+});
+
+// PUT route to edit a post
+router.put('/:id', async (req, res) => {
+  try {
+    if (req.body.text === '') {
+      res
+        .status(400)
+        .json({errorMessage: 'Please provide text to update the post'});
+    } else {
+      const editedPost = await postDb.update(req.params.id, req.body);
+      if (editedPost) {
+        res.status(201).json(editedPost);
+      } else {
+        res.status(404).json({errorMessage: 'Post not found'});
+      }
+    }
+  } catch (error) {
+    res.status(500).json({errorMessage: 'Error updating the post'});
+  }
+});
+
+// DELETE route to delete a post
 router.delete('/:id', async (req, res) => {
   try {
     const deletedPost = await postDb.remove(req.params.id);
