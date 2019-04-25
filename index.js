57dd6ad66bdee8dda31a7f2c7643c0c36121f472
guillsav <guillsav@gmail.com>
2019-04-25 13:48:40 -0400

    port is dynamic

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const server = require('./server.js');
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 server.listen(port, () => {
   console.log(`Server is listening on port: ${port}`);
